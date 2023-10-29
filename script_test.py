d8cc6902ede3d3d47ff71c708dd36867be0d6dce
Zhaopudark <zhaopudark@outlook.com>
2023-10-29 16:52:04 +0800

    add tests for workflows

---
+++ script_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <a>")
+        sys.exit(1)
+
+    # Get the value of 'a' from the command line argument
+    a = sys.argv[1]
+    sys.exit(int(0))
+   
+        
